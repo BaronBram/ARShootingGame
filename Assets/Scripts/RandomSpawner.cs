a0e2e74c8f545f825b4fc14d8a97e483874dd900
BaronBram <nicho.bram@gmail.com>
2023-03-22 21:28:43 +0700

    First Commit

---
+++ Assets/Scripts/RandomSpawner.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomSpawner : MonoBehaviour {
+	public GameObject[] enemies;
+    public int maxtimer = 10;
+    public int mintimer = 3;
+    private float timer;
+    private float timetospawn;
+    public int xmin=-20, xmax=20;
+    public int ymin=-10, ymax=10;//buat bikin semacam area kotak
+    private int objX,objY;
+	// Use this for initialization
+	void Start () {
+		 timetospawn = Random.Range(mintimer, maxtimer); // random awal
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		timer += Time.deltaTime;
+        if(timer > timetospawn){//jika udh saatnya spawn
+            timer = 0; //reset timer
+            timetospawn = Random.Range(mintimer, maxtimer);
+            Debug.Log(timetospawn);
+            RandomPosition();
+            Spawn(Random.Range(0, enemies.Length));//random musuh yg bakal dimunculkan
+
+        }
+	}
+
+	void RandomPosition(){
+        objX = Random.Range(xmin, xmax);
+        objY = Random.Range(ymin, ymax);
+    }
+
+    void Spawn(int index){
+        Instantiate(enemies[index], new Vector3(objX, objY, transform.position.z),transform.rotation); //memunculkan musuh pada index "index"
+    }
+}
---
+++ Assets/Scripts/ShootFire.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootFire : MonoBehaviour 
+{
+ 
+ public int distanceOfRay = 20; // jarak cursor dr kamera ke objek
+ private RaycastHit hit; //menerima objek apa yg bakal kena raycast
+	// Use this for initialization
+	
+
+	// Update is called once per frame
+	void Update () {
+		Ray ray = Camera.main.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0f));//mengeluarkan raycast dr titik tengah camera
+        Debug.DrawLine(transform.position, hit.point, Color.red);
+        if(Physics.Raycast(ray, out hit, distanceOfRay)){//apabila ad sesuatu dr Raycast
+            if(Input.GetButtonDown("Fire1")&&hit.transform.CompareTag("Enemy")){
+                Destroy(hit.transform.gameObject);//destroy sesuatu itu
+                GetComponent<AudioSource>().Play();
+            }
+
+        }
+	}
+}
